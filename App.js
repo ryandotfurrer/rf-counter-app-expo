e766b36e258fa1f0af3cfc71bf8bb15a1d728203
Ryan Furrer <ryandotfurrer@gmail.com>
2024-03-19 15:51:58 -0400

    Initial commit. Created counter

--- App.js
+++ App.js
@@ -1,20 +1,90 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, Button, StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
 
-export default function App() {
+const App= () => {
+  const [count, setCount] = useState(0);
+
+  const subtractOne = () => {
+    setCount(count - 1);
+    // console.log(count);
+  }
+  const addOne = () => {
+    setCount(count + 1);
+    // console.log(count);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Pressable 
+        onPress={subtractOne}
+        aria-label='Subtract One from Count' 
+        style={({pressed}) => [
+          {
+            backgroundColor: pressed ? '#DD92AA' : '#F0D1DB',
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: '100%',
+          },
+          styles.wrapperCustom,
+        ]}>
+        <Text style={styles.buttonText}>-1</Text>
+      </Pressable>
+    <View style={styles.container2}>
+      <Text style={styles.count}>{count}</Text>
+      </View>
+      <Pressable 
+        onPress={addOne}
+        aria-label='Subtract One from Count' 
+        style={({pressed}) => [
+          {
+            backgroundColor: pressed ? '#80ED99' : '#C8F9CD',
+            flex: 1,
+            width: '100%',
+            alignItems: 'center',
+            justifyContent: 'center',
+          },
+          styles.wrapperCustom,
+        ]}>
+        <Text style={styles.buttonText}>+1</Text>
+      </Pressable>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    padding: 16,
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgba(0,0,0,1)',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  container2: {
+    // padding: 16,
+    flex: 1,
+    backgroundColor: 'rgba(255,255,255,0.25)',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 8,
+    padding: 6,
+    margin: 16,
+    width: '100%',
+  },
+  bodyText: {
+    fontSize: 16,
+    color: 'rgba(255,255,255,0.95)',
+  },
+  count: {
+    fontSize: 48,
+    color: 'rgba(255,255,255,0.95)',
+  },
+  wrapperCustom: {
+    borderRadius: 8,
+    padding: 6,
+  },
+  buttonText: {
+    fontSize: 48,}
 });
+
+export default App;
